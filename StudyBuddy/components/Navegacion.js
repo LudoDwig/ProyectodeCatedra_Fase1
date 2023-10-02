deb85d54b84c33265be7f387eb1f11d2ab276fa8
Ludwig Stanley <125160770+LudoDwig@users.noreply.github.com>
2023-10-02 17:27:10 -0600

    modifcando navegacion

--- StudyBuddy/components/Navegacion.js
+++ StudyBuddy/components/Navegacion.js
@@ -4,6 +4,9 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Inicio from './Inicio';
 import BlocNotas from './BlocDeNotas';
 import CronometroApp from './Cronometro';
+import Recordatorio from './Recordatorio';
+
+
 // Importa otras pantallas si es necesario
 
 const Stack = createNativeStackNavigator();
@@ -13,7 +16,11 @@ const NavegacionApp = () =>{
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Inicio" component={Inicio} />
         <Stack.Screen name="Notas" component={BlocNotas} />
-        <Stack.Screen name="Crono" component={CronometroApp}/>
+        <Stack.Screen name="Cronómetro" component={CronometroApp}/>
+        <Stack.Screen name="Recordatorio" component={Recordatorio}/>
+  
+
+        
         {/* Agrega otras pantallas aquí */}
       </Stack.Navigator>
     </NavigationContainer>
